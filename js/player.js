f5aa0315ada777009aab5592fc191343055d1252
Max Tillich <mtillich@gmail.com>
2014-12-28 00:08:03 +0100

    hide toolbars when inactive

--- js/player.js
+++ js/player.js
@@ -17,22 +17,46 @@ $(function() {
     if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement) {
       if (document.documentElement.requestFullscreen) {
         document.documentElement.requestFullscreen();
+        $(this).addClass('isfullscreen');
       } else if (document.documentElement.mozRequestFullScreen) {
         document.documentElement.mozRequestFullScreen();
+        $(this).addClass('isfullscreen');
       } else if (document.documentElement.webkitRequestFullscreen) {
         document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+        $(this).addClass('isfullscreen');
       }
     } else {
       if (document.cancelFullScreen) {
         document.cancelFullScreen();
+        $(this).removeClass('isfullscreen');
       } else if (document.mozCancelFullScreen) {
         document.mozCancelFullScreen();
+        $(this).removeClass('isfullscreen');
       } else if (document.webkitCancelFullScreen) {
         document.webkitCancelFullScreen();
+        $(this).removeClass('isfullscreen');
       }
     }
   });
 
   // FOCUS HUG FOR ANGULAR KEYUP
   $('.hug').focus();
+
+
+
+  var timer;
+
+  $('.ui').addClass('show-ui');
+  setTimeout(function() {
+    $('.ui').removeClass('show-ui');
+    $('html').on('mousemove', function () {
+      $('.ui').addClass('show-ui');
+      try {
+        clearTimeout(timer);
+      } catch (e) {}
+      timer = setTimeout(function () {
+        $('.ui').removeClass('show-ui');
+      }, 1500);
+    });
+  }, 2700);
 });
